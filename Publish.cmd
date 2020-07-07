07afedf327722f0e445131fb4a68696476d16cb5
Ujjwal Singh <ujjwalsingh@outlook.com>
2020-07-08 05:04:38 +0530

    return to project_path right after toggle and status

--- Publish.cmd
+++ Publish.cmd
@@ -2,14 +2,16 @@
 
 call Check
 call Status
+cd %PROJECT_PATH%
+
 if exist "./Config/Project.ini" (
 	set /p ehost=<./Config/Project.ini
 ) else (
 	set ehost="http://localhost"
 	if %HTDOCS_AT_ROOT% equ 0 (
 		call Toggle
+		cd %PROJECT_PATH%
 	)
-	cd %PROJECT_PATH%
 )
 
 powershell -ExecutionPolicy unrestricted -file "%Tiggu%\Script\Publish.ps1" %1 %ehost%
