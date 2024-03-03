12a00833286d415c7ba6d960cea6d0b0ce9825a6
Ujjwal Singh <ujjwalsingh@outlook.com>
2024-03-03 11:39:06 +0530

    Moved build.sh script to project root

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#change working directory to the first argument
-
 cd "$1"
 
 projectRootFile="./Config/Root.ini"
@@ -12,6 +10,6 @@ else
     echo "Root file does not exist"
     projectRootPath="."
 fi
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)/script"
 
 $SCRIPT_DIR/publish.sh prod $(cat ./Config/Project.ini) $projectRootPath
